b7a70189812d36e66cbc3217c5410a851ec61d1b
JoaquinOlivero <joaquinolivero97@gmail.com>
2023-04-22 20:03:29 +0000

    alerts

--- stream/stream.go
+++ stream/stream.go
@@ -3,9 +3,8 @@ package stream
 import (
 	"Twitcher/twitchApi"
 	"bufio"
-	"context"
 	"database/sql"
-	"errors"
+	"encoding/json"
 	"fmt"
 	"image"
 	"image/draw"
@@ -17,15 +16,15 @@ import (
 	"os/exec"
 	"path/filepath"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/golang/freetype/truetype"
 	"github.com/google/renameio"
 	"github.com/nfnt/resize"
+	"github.com/sacOO7/gowebsocket"
 	"golang.org/x/image/font"
 	"golang.org/x/image/math/fixed"
-	"nhooyr.io/websocket"
-	"nhooyr.io/websocket/wsjson"
 )
 
 type Song struct {
@@ -74,10 +73,30 @@ type SubscriptionChannel struct {
 }
 
 func Twitch(streamUrl string) error {
-	// Pipe the songs to the stdin of the ffmpeg instance that combines audio and video.
-	pr, pw, err := os.Pipe()
+	// Named pipes
+	audioPipePath := "files/stream/audio"
+	videoPipePath := "files/stream/video"
+
+	// Remove the named pipes if they already exists.
+	err := os.Remove(audioPipePath)
+	if err != nil && !os.IsNotExist(err) {
+		panic(err)
+	}
+
+	err = os.Remove(videoPipePath)
+	if err != nil && !os.IsNotExist(err) {
+		panic(err)
+	}
+
+	// Create named pipes.
+	err = syscall.Mkfifo(audioPipePath, 0777)
 	if err != nil {
-		return err
+		panic(err)
+	}
+
+	err = syscall.Mkfifo(videoPipePath, 0777)
+	if err != nil {
+		panic(err)
 	}
 
 	// Infinite loop through songs and buffering the audio files in real-time to the "pw" pipe.
@@ -93,6 +112,11 @@ func Twitch(streamUrl string) error {
 
 			// Loop through the songs in real-time.
 			for _, song := range songs {
+				// Open named audio pipe
+				audioPipe, err := os.OpenFile("files/stream/audio", os.O_RDWR, os.ModeNamedPipe)
+				if err != nil {
+					panic(err)
+				}
 
 				// 2 second silent audio.
 				anullsrc, err := os.Open("files/songs/silence.mp3")
@@ -125,12 +149,13 @@ func Twitch(streamUrl string) error {
 					}
 
 					// process buf
-					_, err = pw.Write(buffer)
+					_, err = audioPipe.Write(buffer)
 					if err != nil {
 						return err
 					}
 
 				}
+				r.Reset(r)
 				anullsrc.Close()
 
 				// Change cover
@@ -166,14 +191,14 @@ func Twitch(streamUrl string) error {
 					}
 
 					// process buf
-					_, err = pw.Write(buffer)
+					_, err = audioPipe.Write(buffer)
 					if err != nil {
 						return err
 					}
 
 				}
+				r.Reset(r)
 				file.Close()
-
 			}
 
 			fmt.Println("Songs loop ending. Starting a new one")
@@ -182,117 +207,133 @@ func Twitch(streamUrl string) error {
 	}()
 
 	// Go routine to handle alert notifications. The notifications come from Twitch's websocket api.
+	var wgAlerts sync.WaitGroup
+	wgAlertsI := 1
+	wgAlerts.Add(wgAlertsI)
+
+	alertFrame := 0
+
 	go func() error {
 		var wg sync.WaitGroup
 
 		// Go routine to validate access token and refresh it if necessary.
 		// To subscribe to Twitch's websocket events it is required to have a valid user access token.
-		i := 1
-		wg.Add(i)
-		go func() error {
+		wgI := 1
+		wg.Add(wgI)
+
+		go func() {
 			for {
+
 				token, err := twitchApi.ValidateToken()
 				if err != nil {
 					log.Println(err)
-					return err
 				}
 
 				// Once there is a valid access token. Decrement the waitgroup count to zero and unblock the connection to Twitch's websocket server.
-				if i == 1 {
+				if wgI == 1 {
 					wg.Done()
-					i--
+					wgI--
 				}
 
 				// Hold loop until 10 minutes before the access token expires and then refresh the token.
 				if token.ExpiresIn > 10*60 {
 					sleepDuration := time.Duration(token.ExpiresIn - (10 * 60))
 					time.Sleep(sleepDuration * time.Second)
+					log.Println("Access token will expire in ten minutes. Getting a new one.")
 				}
 
 				twitchApi.RefreshToken()
 			}
-
 		}()
 
 		// wait until there is a valid acces token availabe to use.
 		wg.Wait()
 
-		// Needs to be in a for loop because the connection to the websocket server could fail and needs to be tried again.
+		// channels that will contain the type of notification and the username.
+		channel := make(chan SubscriptionChannel, 300)
+		exit := make(chan struct{}, 1)
+
+		// Go routine to catch notifications coming from the websocket connection to Twitch.
+		go func() {
+		alert:
+			for {
+				select {
+				case notification := <-channel:
+
+					// new follower alert. Draw the username on top of the alert with a fade in effect.
+					if notification.Type == "channel.follow" {
+						cmd := exec.Command("ffmpeg", "-hide_banner", "-re", "-c:v", "libvpx-vp9", "-i", "files/stream/alerts/follower-empty.webm",
+							"-filter_complex", "[0:v]drawtext=fontfile=../../../Poppins-Bold.ttf:text='"+notification.Username+"':fontsize=16:fontcolor=ffffff:alpha='if(lt(t,0.5),0,if(lt(t,1.5),(t-0.5)/1,if(lt(t,10.5),1,if(lt(t,11),(0.5-(t-10.5))/0.5,0))))':x=(w-text_w)/2:y=(h-text_h)/2",
+							"files/stream/alerts/frames/%d.png")
+
+						cmd.Run()
+					}
+
+					// new sub alert. Draw the username on top of the alert with a fade in effect.
+					if notification.Type == "channel.subscribe" {
+						cmd := exec.Command("ffmpeg", "-hide_banner", "-re", "-c:v", "libvpx-vp9", "-i", "files/stream/alerts/sub-empty.webm",
+							"-filter_complex", "[0:v]drawtext=fontfile=../../../Poppins-Bold.ttf:text='"+notification.Username+"':fontsize=16:fontcolor=ffffff:alpha='if(lt(t,0.5),0,if(lt(t,1.5),(t-0.5)/1,if(lt(t,10.5),1,if(lt(t,11),(0.5-(t-10.5))/0.5,0))))':x=(w-text_w)/2:y=(h-text_h)/2",
+							"files/stream/alerts/frames/%d.png")
+
+						cmd.Run()
+					}
+
+					// Frame where the alert should stop and go back to showing a transparent png image as the alert placeholder.
+					for {
+						if alertFrame >= 235 {
+							break
+						}
+					}
+
+					// Transparent placeholder.
+					cmd := exec.Command("ffmpeg", "-hide_banner", "-re", "-c:v", "libvpx-vp9", "-i", "files/stream/alerts/empty.webm", "files/stream/alerts/frames/%d.png")
+
+					cmd.Run()
+
+				case <-exit:
+					break alert
+				}
+			}
+		}()
+
 		var wsURL string
 		wsURL = "wss://eventsub.wss.twitch.tv/ws"
+		// wsURL = "ws://127.0.0.1:8080/ws"
+		reconnect := make(chan struct{}, 1)
 
-	websocket:
 		for {
-			// channels that will contain the type of notification and the username.
-			channel := make(chan SubscriptionChannel, 300)
-			exit := make(chan bool, 300)
 
-			// Go routine to catch notifications coming from the websocket connection to Twitch.
-			go func() {
-			alert:
-				for {
-					select {
-					case alert := <-channel:
-						err := changeAlert(alert)
-						if err != nil {
-							log.Println(err)
-						}
+			//Create a client instance
+			socket := gowebsocket.New(wsURL)
 
-						time.Sleep(9 * time.Second)
-
-						alert.Type = "reset"
-						err = changeAlert(alert)
-						if err != nil {
-							log.Println(err)
-						}
-
-					case <-exit:
-						break alert
-					}
-				}
-			}()
-
-			// Connect to Twitch's ws server and receive real-time notifications.
-			ctx := context.Background()
-
-			c, _, err := websocket.Dial(ctx, wsURL, nil)
-			if err != nil {
-				log.Println("Couldn't connect to " + wsURL + " will retry in 60 seconds.")
-				log.Println("error: ", err)
-				time.Sleep(60 * time.Second)
-
-				continue
+			socket.OnConnected = func(socket gowebsocket.Socket) {
+				log.Println("Connected to server: ", wsURL)
 			}
-			defer c.Close(websocket.StatusInternalError, "the sky is falling")
 
-			log.Println("Connected to: ", wsURL)
-			// For loop used to constantly read for websocket incoming connections.
-		notifications:
-			for {
+			socket.OnConnectError = func(err error, socket gowebsocket.Socket) {
+				log.Println("Received connect error ", err)
+			}
+
+			// Read received messages.
+			socket.OnTextMessage = func(message string, socket gowebsocket.Socket) {
 				var v Response
-				err = wsjson.Read(ctx, c, &v)
+				err = json.Unmarshal([]byte(message), &v)
 				if err != nil {
-					if err.Error()[len(err.Error())-50:] == "WebSocket closed: failed to read frame header: EOF" { // This error happens when the websocket connection has been closed server side. For example when Twitch's websocket servers are down.
-						log.Println("Couln't read from websocket. Will try to reconnect in 10 seconds.")
-						log.Println("error: ", err)
-						time.Sleep(10 * time.Second)
-						break notifications
-					}
-					time.Sleep(10 * time.Second)
 					log.Println(err)
-					continue notifications
 				}
 
 				// When connection to wss://eventsub.wss.twitch.tv/ws is established, Twitch replies with a welcome message that contains a session_id needed to subscribe to events.
 				if v.Metadata.MessageType == "session_welcome" {
 					// Send POST requests to create EventSub subscriptions for channel.follow and channel.subscribe.
 					err := twitchApi.CreateEventSubs(v.Payload.Session.Id, "https://api.twitch.tv/helix/eventsub/subscriptions")
+					// err := twitchApi.CreateEventSubs(v.Payload.Session.Id, "http://127.0.0.1:8080/eventsub/subscriptions")
 					if err != nil {
-						time.Sleep(10 * time.Second)
-						c.Close(websocket.StatusNormalClosure, "")
-						exit <- true
 						log.Println(err)
-						break notifications
+					}
+
+					if wgAlertsI == 1 {
+						wgAlerts.Done()
+						wgAlertsI--
 					}
 				}
 
@@ -310,17 +351,24 @@ func Twitch(streamUrl string) error {
 				// Twitch sends notification if the edge server the client is connected to needs to be swapped.
 				if v.Metadata.MessageType == "session_reconnect" {
 					wsURL = v.Payload.Session.ReconnectURL
-					c.Close(websocket.StatusNormalClosure, "")
-					continue websocket
+					log.Println("Request from Twitch to reconnect websocket client to: ", wsURL)
+					reconnect <- struct{}{}
 				}
-
 			}
 
-			c.Close(websocket.StatusNormalClosure, "")
+			// This will send websocket handshake request to socketcluster-server
+			socket.Connect()
+
+			// Close socket connection and continue loop to reconnect to new server address.
+			// This channel waiting also blocks the infinite loop from continuing, allowing to maintain the websocket connection and create a new one when needed.
+			<-reconnect
+			log.Println("reconnecting...")
+			socket.Close()
+			continue
 		}
 	}()
 
-	time.Sleep(1 * time.Second)
+	wgAlerts.Wait()
 
 	// Pipe the video and audio ready to be streamed.
 	spr, spw, err := os.Pipe()
@@ -332,38 +380,52 @@ func Twitch(streamUrl string) error {
 	// This ffmpeg instance pipes its output to stdout to later be used in the actual ffmpeg instance that has the job to stream the input to Twitch's rtmp server.
 	go func() error {
 
-		cmd := exec.Command("ffmpeg",
-			"-hide_banner",
-			"-y", "-re",
-			"-stream_loop", "-1",
-			"-i", "files/stream/sunset-720p.mp4", // Background video
-			"-f", "image2", "-loop", "1", "-i", "files/stream/stream.png", // Overlay that shows the song's cover. The "stream.png" file will be atomically changed according to the song that is being currently played.
-			"-stream_loop", "-1", "-c:v", "libvpx-vp9", "-f", "concat", "-i", "files/stream/alerts/list1.txt", // Overlay that shows the alerts. Alerts are video files that use the ".webm" format.
-			"-filter_complex", "[0][1]overlay=5:5[v1];[v1][2]overlay=W-w+10:H-h+60", // Filter that actually places the overlays over the video.
-			"-i", "pipe:0", // Audio input pipe.
-			"-c:v", "libx264", // Encode new video with overlays.
-			"-c:a", "copy", // Copy the single audio stream.
-			"-g", "50",
-			"-keyint_min", "50", "-force_key_frames", "expr:gte(t,n_forced*2)",
-			"-f", "flv", "-flvflags", "no_duration_filesize", "-", // Pipe the result to the ffmpeg instance stdout.
-		)
+		for {
 
-		// cmd.Stderr = os.Stderr // ffmpeg logs everything to stderr.
+			cmd := exec.Command("ffmpeg",
+				"-hide_banner",
+				"-y", "-re",
+				"-stream_loop", "-1",
+				"-i", "files/stream/sunset-720p.mp4", // Background video
+				"-f", "image2", "-loop", "1", "-i", "files/stream/stream.png", // Overlay that shows the song's cover. The "stream.png" file will be atomically changed according to the song that is being currently played.
+				"-f", "image2", "-loop", "1", "-i", "files/stream/alerts/frames/%d.png",
+				"-filter_complex", "[0][1]overlay=5:5[v1];[v1][2]overlay=W-w+10:H-h+60", // Filter that actually places the overlays over the video.
+				"-i", "files/stream/audio", // Audio input pipe.
+				"-c:v", "libx264", // Encode new video with overlays.
+				"-c:a", "copy", // Copy the single audio stream.
+				"-g", "50",
+				"-keyint_min", "50", "-force_key_frames", "expr:gte(t,n_forced*2)",
+				"-f", "flv", "-flvflags", "no_duration_filesize", "-", // Pipe the result to the ffmpeg instance stdout.
+				// "-f", "flv", "-flvflags", "no_duration_filesize", streamUrl,
+			)
 
-		cmd.Stdin = pr   // Pipe that contains the buffered songs.
-		cmd.Stdout = spw // Write the stdout the the "spw" pipe.
+			cmd.Stderr = os.Stderr // ffmpeg logs everything to stderr.
 
-		err = cmd.Start()
-		if err != nil {
-			return err
+			cmd.Stdout = spw // Write the stdout the the "spw" pipe.
+
+			err = cmd.Start()
+			if err != nil {
+				return err
+			}
+
+			go func() {
+				for {
+					if alertFrame == 250 {
+						alertFrame = 1
+						continue
+					}
+					alertFrame += 1
+					time.Sleep(40 * time.Millisecond)
+				}
+			}()
+
+			err = cmd.Wait()
+			if err != nil {
+				log.Fatalln(err)
+				// return err
+			}
 		}
 
-		err = cmd.Wait()
-		if err != nil {
-			return err
-		}
-
-		return nil
 	}()
 
 	// Ffmpeg instance that streams the piped input to Twitch's rtmp servers.
@@ -378,8 +440,8 @@ func Twitch(streamUrl string) error {
 		streamUrl,
 	)
 
-	cmd.Stderr = os.Stderr // ffmpeg logs everything to stderr.
-	cmd.Stdin = spr        // Pipe that contains the video and audio ready to be streamed.
+	// cmd.Stderr = os.Stderr // ffmpeg logs everything to stderr.
+	cmd.Stdin = spr // Pipe that contains the video and audio ready to be streamed.
 
 	cmd.Run()
 	return nil
@@ -412,6 +474,8 @@ func getSongs() ([]Song, error) {
 
 		songs = append(songs, song)
 	}
+
+	db.Close()
 
 	return songs, nil
 }
@@ -504,6 +568,9 @@ func changeCover(name, author, page, cover string) error {
 
 	renameio.WriteFile("files/stream/stream.png", overlay, 0644)
 
+	file.Close()
+	output.Close()
+
 	return nil
 }
 
@@ -528,146 +595,6 @@ func addText(img *image.RGBA, text string, fontFamily []byte, fontSize float64, 
 		Dot:  fixed.P(x, y),
 	}
 	d.DrawString(text)
-
-	return nil
-}
-
-// Change the location of the file being used as an alert in list1.txt and list2.txt files.
-func changeAlert(alert SubscriptionChannel) error {
-
-	if alert.Type == "channel.follow" {
-		err := createAlert(alert.Username, "files/stream/alerts/follower-empty.webm", "files/stream/alerts/follower.webm")
-		if err != nil {
-			return err
-		}
-
-		err = replaceLine("files/stream/alerts/list1.txt", "follower.webm")
-		if err != nil {
-			return err
-		}
-
-		err = replaceLine("files/stream/alerts/list2.txt", "follower.webm")
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	if alert.Type == "channel.subscribe" {
-		err := createAlert(alert.Username, "files/stream/alerts/sub-empty.webm", "files/stream/alerts/sub.webm")
-		if err != nil {
-			return err
-		}
-		err = replaceLine("files/stream/alerts/list1.txt", "sub.webm")
-		if err != nil {
-			return err
-		}
-
-		err = replaceLine("files/stream/alerts/list2.txt", "sub.webm")
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	// Reset the list1.txt and list2.txt files to use the default empty.webm. This empty.webm file works as a transparent placeholder for the actual alerts.
-	if alert.Type == "reset" {
-		err := replaceLine("files/stream/alerts/list1.txt", "empty.webm")
-		if err != nil {
-			return err
-		}
-
-		err = replaceLine("files/stream/alerts/list2.txt", "empty.webm")
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	return nil
-}
-
-// FFmpeg command that inserts text into video.
-func createAlert(username, input, output string) error {
-	// Add ellipsis to username if it is too long. Max 17 characters.
-	maxLength := 17
-	if len(username) > maxLength {
-		username = username[:maxLength] + "..."
-	}
-
-	// ffmpegCommand := `-c:v libvpx-vp9 -i ` + input + ` -filter_complex "[0:v]drawtext=fontfile=Poppins-Bold.ttf:text='` + username + `':fontsize=16:fontcolor=ffffff:alpha='if(lt(t,0.5),0,if(lt(t,1.5),(t-0.5)/1,if(lt(t,10.5),1,if(lt(t,11),(0.5-(t-10.5))/0.5,0))))':x=(w-text_w)/2:y=(h-text_h)/2" ` + output + ` -y`
-	// ffmpeg -c:v libvpx-vp9 -i sub.webm -filter_complex "[0:v]drawtext=fontfile=../../../Poppins-Bold.ttf:text='TestUserTwitchAAA...':fontsize=16:fontcolor=ffffff:alpha='if(lt(t,0.5),0,if(lt(t,1.5),(t-0.5)/1,if(lt(t,10.5),1,if(lt(t,11),(0.5-(t-10.5))/0.5,0))))':x=(w-text_w)/2:y=(h-text_h)/2"
-	cmd := exec.Command("ffmpeg",
-		"-c:v", "libvpx-vp9", "-i", input,
-		"-filter_complex", "[0:v]drawtext=fontfile=Poppins-Bold.ttf:text='"+username+"':fontsize=16:fontcolor=ffffff:alpha='if(lt(t,0.5),0,if(lt(t,1.5),(t-0.5)/1,if(lt(t,10.5),1,if(lt(t,11),(0.5-(t-10.5))/0.5,0))))':x=(w-text_w)/2:y=(h-text_h)/2",
-		output, "-y",
-	)
-
-	err := cmd.Run()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func replaceLine(filePath, gif string) error {
-	// Open the text file for reading and writing
-	file, err := os.OpenFile(filePath, os.O_RDWR, 0644)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// Create a scanner to read the file line by line
-	scanner := bufio.NewScanner(file)
-
-	// Create a buffer to store the lines of the file
-	var lines []string
-
-	// Read the lines from the file and store them in the buffer
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	// Check if the file has at least two lines
-	if len(lines) < 2 {
-		err := errors.New("file does not have at least two lines")
-		return err
-	}
-
-	// Replace the second line with a new line
-	lines[1] = "file " + "'" + gif + "'"
-
-	// Truncate the file to 0 bytes
-	err = file.Truncate(0)
-	if err != nil {
-		return err
-	}
-
-	// Rewind the file to the beginning
-	_, err = file.Seek(0, 0)
-	if err != nil {
-		return err
-	}
-
-	// Write the updated lines to the file
-	writer := bufio.NewWriter(file)
-	for _, line := range lines {
-		_, err := writer.WriteString(line + "\n")
-		if err != nil {
-			return err
-		}
-	}
-
-	// Flush the writer to write the changes to disk
-	err = writer.Flush()
-	if err != nil {
-		return err
-	}
 
 	return nil
 }
